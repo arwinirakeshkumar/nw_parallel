476e539b1791c810c043544230708c870829f210
hallundbaek <tobias.h.petersen@gmail.com>
2016-10-17 20:18:54 +1100

    openmp, done, maybe needs tuning

--- nw-openmp.cpp
+++ nw-openmp.cpp
@@ -8,8 +8,8 @@ using namespace std;
 #define MATCH 1
 #define MISMATCH 0
 #define INDEL -1
-#define TILESIZE 16
-#define MAXSIZE 1024
+#define TILESIZE 64
+#define MAXSIZE 10240
 #define CORES 4
 
 void nw_tile(int x_vals[], int y_vals[], const char x_chars[], const char y_chars[], int x_size, int y_size, int corner) {
@@ -95,11 +95,16 @@ int main(int argc, char *argv[]) {
   int x_vals[MAXSIZE];
   int y_vals[MAXSIZE];
   int corners[MAXSIZE/TILESIZE][MAXSIZE/TILESIZE];
+  int tile_y, tile_x;
   for (int i = 0; i < MAXSIZE; i++) {
     x_vals[i] = (i + 1) * INDEL;
     y_vals[i] = x_vals[i];
   }
-  nw_tile(x_vals, y_vals, x_chars, y_chars, TILESIZE, TILESIZE, 0);
+  tile_x = TILESIZE;
+  tile_y = TILESIZE;
+  if (x_size < TILESIZE) tile_x = x_size;
+  if (y_size < TILESIZE) tile_y = y_size;
+  nw_tile(x_vals, y_vals, x_chars, y_chars, tile_x, tile_y, 0);
   corners[1][1] = x_vals[TILESIZE-1];
   int x_tiles = ((TILESIZE + x_size - 1) / TILESIZE) - 1;
   int y_tiles = ((TILESIZE + y_size - 1) / TILESIZE) - 1;
@@ -113,7 +118,6 @@ int main(int argc, char *argv[]) {
   int max_i;
   int offset = 1;
   int j;
-  int tile_y, tile_x;
   int edge_x = x_size % TILESIZE;
   int edge_y = y_size % TILESIZE;
   if (!edge_x) edge_x = TILESIZE;
@@ -132,21 +136,20 @@ int main(int argc, char *argv[]) {
         nw_tile(&x_vals[x_offset], &y_vals[y_offset], &x_chars[x_offset], &y_chars[y_offset], edge_x, edge_y, corners[j][i-j]);
         break;
       }
-      for (; j <= max_i; j++) {
-        x_offset = TILESIZE*j;
-        y_offset = TILESIZE*(i-j);
+#pragma omp parallel for shared(x_vals, y_vals, x_chars, y_chars, corners)  private(x_offset, y_offset, tile_x, tile_y)
+      for (int k = j; k <= max_i; k++) {
+        x_offset = TILESIZE*k;
+        y_offset = TILESIZE*(i-k);
         tile_x = TILESIZE;
         tile_y = TILESIZE;
-        if (j == x_tiles) tile_x = edge_x;
-        if (i-j == y_tiles) tile_y = edge_y;
-        //printf("%d, %d\n", x_offset, y_offset);
-        //printf("corner: %d, j: %d, i-j: %d\n", corners[j][i-j], j, i-j);
-        nw_tile(&x_vals[x_offset], &y_vals[y_offset], &x_chars[x_offset], &y_chars[y_offset], tile_x, tile_y, corners[j][i-j]);
-        corners[j+1][i-j+1] = x_vals[x_offset + TILESIZE - 1];
+        if (k == x_tiles) tile_x = edge_x;
+        if (i-k == y_tiles) tile_y = edge_y;
+        nw_tile(&x_vals[x_offset], &y_vals[y_offset], &x_chars[x_offset], &y_chars[y_offset], tile_x, tile_y, corners[k][i-k]);
+        corners[k+1][i-k+1] = x_vals[x_offset + TILESIZE - 1];
       }
     }
   }
-/*  for (int i = 0; i < x_size; i++) {
+  /*for (int i = 0; i < x_size; i++) {
     printf("%d ", x_vals[i]);
   }
   printf("\n");
